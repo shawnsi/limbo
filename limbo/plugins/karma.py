4aca7b64dea3633e6b9afa181dda1d71c9024212
Shawn Siefkas <shawn.siefkas@meredith.com>
2015-04-30 13:33:48 -0500

    Make all attempts to set self karma bump down

--- limbo/plugins/karma.py
+++ limbo/plugins/karma.py
@@ -3,13 +3,13 @@ import re
 import shelve
 
 
-def update_karma(user, operation):
+def update_karma(user, sender, operation):
     karma_dict = shelve.open('./karma.shelf')
     key = user.encode('ascii')
     karma = karma_dict.setdefault(key, 0)
     bumps = len(operation) - 1
 
-    if operation.startswith('+'):
+    if operation.startswith('+') and user != sender:
         karma += bumps
     else:
         karma -= bumps
@@ -22,8 +22,9 @@ def update_karma(user, operation):
 def on_message(msg, server):
     text = msg.get("text", "")
 
+    status = []
+
     if text.startswith('!karma'):
-        status = []
 
         karma_dict = shelve.open('./karma.shelf')
         for user, karma in karma_dict.items():
@@ -32,15 +33,14 @@ def on_message(msg, server):
         karma_dict.close()
         return '\n'.join(status)
 
-    updates = []
-
     matches = re.findall(r"@([^ +\-@]+)\>(\+{2,}|-{2,})", text)
     if not matches:
         return
 
     for user_id, operation in matches:
         user = server.slack.server.users.get(user_id)["name"]
-        karma = update_karma(user, operation)
-        updates.append('%s now has %s karma' % (user, karma))
+        sender = server.slack.server.users.get(msg.get("user"))["name"]
+        karma = update_karma(user, sender, operation)
+        status.append('%s now has %s karma' % (user, karma))
 
-    return '\n'.join(updates)
+    return '\n'.join(status)
